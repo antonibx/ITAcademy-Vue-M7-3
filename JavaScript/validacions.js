308df5bf83ac88ccc377701972b6787aec4be51a
Antoni Blanche <antoni.bx@gmail.com>
2021-06-03 15:42:34 +0200

    Refactorizacion i control de existentes

--- JavaScript/validacions.js
+++ JavaScript/validacions.js
@@ -2,19 +2,14 @@
 var bbdd = new Map();
 
 // Dades de prova: (usuari, [correu, contrassenya, província])
-bbdd.set("usuari1", ["Nom 1", "prova@prova.cat", "Pass0001", "Barcelona"]);
-bbdd.set("usuari2", ["Nom 2", "prova@prova.cat", "Pass0002", "Girona"]);
-bbdd.set("usuari3", ["Nom 3", "prova@prova.cat", "Pass0003", "Tarragona"]);
-bbdd.set("usuari4", ["Nom 4", "prova@prova.cat", "Pass0004", "Lleida"]);
+bbdd.set("usuari1", ["Nom 1", "prova1@prova.cat", "Pass0001", "Barcelona"]);
+bbdd.set("usuari2", ["Nom 2", "prova2@prova.cat", "Pass0002", "Girona"]);
+bbdd.set("usuari3", ["Nom 3", "prova3@prova.cat", "Pass0003", "Tarragona"]);
+bbdd.set("usuari4", ["Nom 4", "prova4@prova.cat", "Pass0004", "Lleida"]);
 
 // ---------------------------------------------------------------------------------------
 // BOTÓ 1: REGISTRE
 // ---------------------------------------------------------------------------------------
-
-// Definim el formulari i els seus inputs
-var formulari = document.getElementById('formulari');
-var inputs = document.querySelectorAll('#formulari input');
-var login = document.getElementById('formularilogin');
 
 // Escribim les expressions regulars que ha de complir cada camp
 const expresiones = {
@@ -25,141 +20,150 @@ const expresiones = {
 };
 
 // Inicialitzem tots els camps amb expressions regulars a incorrectes
-const campos = {usuari: false, nom: false, pass: false, mail: false};
+const campos = {Usuari: false, Nom: false, Pass: false, Mail: false};
 
 // EventListeners pels Inputs
-inputs.forEach((input) => {
+document.querySelectorAll('#formulari input').forEach((input) => {
 	input.addEventListener('keyup', validarFormulari); //Quan escribim una lletra
 	input.addEventListener('blur', validarFormulari);  //Quan per el focus
 });
 
 // EventListeners pel Formulari complet
-formulari.addEventListener('submit', (e) => {
+function Formulari (e) {
 	e.preventDefault();
-	let condicions = document.getElementById('condicions');
-	let provincia = document.getElementById('provincia').value;
-	if(campos.usuari && campos.nom && campos.pass && campos.mail && comprovaProvincia(provincia, 'provincia') && condicions.checked ){
+	let usuari = e.target[0].value;
+	let mail = e.target[4].value;
+	let provincia = e.target[5];
+	let condicions = e.target[6];
+	
+	if (jaExisteix(usuari, mail)) { 
+		//Si l'usuari ja és a la base de dades (per user o mail) mostrem un avís
+		e.target.children[0].children[7].classList.add('formulariMensaje-actiu'); //formulariJaExisteix
+	} else if(campos.Usuari && campos.Nom && campos.Pass && campos.Mail && validarProvincia(provincia) && condicions.checked){
 		//Guardem a la base de dades
-		let usuari = document.getElementById("usuari").value;
-		let nom = document.getElementById("nom").value;
-		let mail = document.getElementById("mail").value;
-		let pass = document.getElementById("pass").value;
-		console.log("1 "+usuari+" 2 "+nom+" 3 "+mail+" 4 "+pass+" 5 "+provincia);
-        bbdd.set(usuari, [nom, mail, pass, provincia]);
-		document.getElementById("dades-registre").innerHTML = "<b> > Usuari:</b> " + usuari + "<br> > <b>Nom:</b> " + nom + "<br> > <b>Mail:</b> " + mail + "<br> > <b>Pass:</b> " + pass + "<br> > <b>Provincia:</b> "+provincia;
-
-		//mostrem modal
-		formulari.reset();
-		document.getElementById('formulari__mensaje').classList.remove('formulari__mensaje-actiu');
-		document.getElementById('formulari__mensaje-exito').classList.add('formulari__mensaje-exito-actiu');
+		let nom = e.target[1].value;
+		let pass = e.target[2].value;
+        bbdd.set(usuari, [nom, mail, pass, provincia.value]);
+		e.target.parentElement.parentElement.children[4].children[0].children[0].children[1].children[1].innerHTML = "<b> > Usuari:</b> " + usuari + "<br> > <b>Nom:</b> " + nom + "<br> > <b>Mail:</b> " + mail + "<br> > <b>Pass:</b> " + pass + "<br> > <b>Provincia:</b> "+provincia.value;
+		
+		//Eliminem els camps i possibles missatges d'error anteriors
+		e.target.reset();
+		e.target.children[0].children[8].classList.remove('formulariMensaje-actiu');
+		e.target.children[0].children[8].classList.remove('formulariJaexisteix-actiu');
+		e.target.children[1].children[0].children[1].classList.add('formulariMensaje-exito-actiu');
 		setTimeout(() => {
-			document.getElementById('formulari__mensaje-exito').classList.remove('formulari__mensaje-exito-actiu');
+			e.target.children[1].children[0].children[1].classList.remove('formulariMensaje-exito-actiu');
 		}, 5000);
-
-		document.querySelectorAll('.formulari__grup-correcte').forEach((icona) => {
-			icona.classList.remove('formulari__grup-correcte');
+		document.querySelectorAll('.formulariGrup-correcte').forEach((icona) => {
+			icona.classList.remove('formulariGrup-correcte');
 		});
+
+		//Mostrem la finestra modal
 		$('#registre-exitos').modal('show');
 		$('#registre').collapse('hide');
 	} else {
-		document.getElementById('formulari__mensaje').classList.add('formulari__mensaje-actiu');
-	}
-});
-
-// Funció principal
-function validarFormulari (e) {
-	switch (e.target.name) {
-		case "usuari":
-			validarCampo(expresiones.usuari, e.target, 'usuari');
-		break;
-		case "nom":
-			validarCampo(expresiones.nom, e.target, 'nom');
-		break;
-		case "pass":
-			validarCampo(expresiones.pass, e.target, 'pass');
-			validarPassword2();
-		break;
-		case "pass2":
-			validarPassword2();
-		break;
-		case "mail":
-			validarCampo(expresiones.mail, e.target, 'mail');
-		break;
+		e.target.children[0].children[8].classList.add('formulariMensaje-actiu');
 	}
 }
 
+// Funcions de validació
+function validarFormulari(e) {
+	switch (e.target.name) {
+		case "usuari":
+			validarCampo(expresiones.usuari, e.target, "Usuari");
+		break;
+		case "nom":
+			validarCampo(expresiones.nom, e.target, "Nom");
+		break;
+		case "pass":
+			validarCampo(expresiones.pass, e.target, "Pass");
+			validarPassword2(e.target, e.target.parentElement.parentElement.parentElement.children[3].children[1].children[0]);
+		break;
+		case "pass2":
+			validarPassword2(e.target.parentElement.parentElement.parentElement.children[2].children[1].children[0], e.target);
+		break;
+		case "mail":
+			validarCampo(expresiones.mail, e.target, "Mail");
+		break;
+	}
+}
 function validarCampo (expresion, input, campo) {
 	if(expresion.test(input.value)){
-		document.getElementById(`grup__${campo}`).classList.remove('formulari__grup-incorrecte');
-		document.getElementById(`grup__${campo}`).classList.add('formulari__grup-correcte');
-		document.querySelector(`#grup__${campo} i`).classList.add('fa-check-circle');
-		document.querySelector(`#grup__${campo} i`).classList.remove('fa-times-circle');
-		document.querySelector(`#grup__${campo} .formulari__input-error`).classList.remove('formulari__input-error-actiu');
+		input.parentElement.parentElement.classList.remove('formulariGrup-incorrecte');
+		input.parentElement.parentElement.classList.add('formulariGrup-correcte');
+		input.parentElement.children[1].classList.add('fa-check-circle');
+		input.parentElement.children[1].classList.remove('fa-times-circle');
+		input.parentElement.parentElement.children[2].classList.remove('formulariInput-error-actiu');
 		campos[campo] = true;
 	} else {
-		document.getElementById(`grup__${campo}`).classList.add('formulari__grup-incorrecte');
-		document.getElementById(`grup__${campo}`).classList.remove('formulari__grup-correcte');
-		document.querySelector(`#grup__${campo} i`).classList.add('fa-times-circle');
-		document.querySelector(`#grup__${campo} i`).classList.remove('fa-check-circle');
-		document.querySelector(`#grup__${campo} .formulari__input-error`).classList.add('formulari__input-error-actiu');
+		input.parentElement.parentElement.classList.add('formulariGrup-incorrecte');
+		input.parentElement.parentElement.classList.remove('formulariGrup-correcte');
+		input.parentElement.children[1].classList.add('fa-times-circle');
+		input.parentElement.children[1].classList.remove('fa-check-circle');
+		input.parentElement.parentElement.children[2].classList.add('formulariInput-error-actiu');
 		campos[campo] = false;
 	}
 }
-
-function comprovaProvincia (input, campo) {
+function validarProvincia(input) {
 	let provinciaOK;
-	if(input !== "" && input !== undefined & input !== null) {
-		document.getElementById(`grup__${campo}`).classList.remove('formulari__grup-incorrecte');
-		document.getElementById(`grup__${campo}`).classList.add('formulari__grup-correcte');
-		document.querySelector(`#grup__${campo} i`).classList.add('fa-check-circle');
-		document.querySelector(`#grup__${campo} i`).classList.remove('fa-times-circle');
-		document.querySelector(`#grup__${campo} .formulari__input-error`).classList.remove('formulari__input-error-actiu');
+	if(input.value !== "" && input.value !== undefined & input.value !== null) {
+		input.parentElement.parentElement.classList.remove('formulariGrup-incorrecte');
+		input.parentElement.parentElement.classList.add('formulariGrup-correcte');
+		input.parentElement.children[1].classList.add('fa-check-circle');
+		input.parentElement.children[1].classList.remove('fa-times-circle');
+		input.parentElement.parentElement.children[2].classList.remove('formulariInput-error-actiu');
 		provinciaOK = true;
 	} else {
-		document.getElementById(`grup__${campo}`).classList.add('formulari__grup-incorrecte');
-		document.getElementById(`grup__${campo}`).classList.remove('formulari__grup-correcte');
-		document.querySelector(`#grup__${campo} i`).classList.add('fa-times-circle');
-		document.querySelector(`#grup__${campo} i`).classList.remove('fa-check-circle');
-		document.querySelector(`#grup__${campo} .formulari__input-error`).classList.add('formulari__input-error-actiu');
+		input.parentElement.parentElement.classList.add('formulariGrup-incorrecte');
+		input.parentElement.parentElement.classList.remove('formulariGrup-correcte');
+		input.parentElement.children[1].classList.add('fa-times-circle');
+		input.parentElement.children[1].classList.remove('fa-check-circle');
+		input.parentElement.parentElement.children[2].classList.add('formulariInput-error-actiu');
 		provinciaOK = false;
 	}
 	return provinciaOK;
 }
-
-function validarPassword2() {
-	let inputPassword1 = document.getElementById('pass');
-	let inputPassword2 = document.getElementById('pass2');
-
+function validarPassword2(inputPassword1, inputPassword2) {
 	if(inputPassword1.value !== inputPassword2.value){
-		document.getElementById("grup__pass2").classList.add("formulari__grup-incorrecte");
-		document.getElementById("grup__pass2").classList.remove("formulari__grup-correcte");
-		document.querySelector("#grup__pass2 i").classList.add("fa-times-circle");
-		document.querySelector("#grup__pass2 i").classList.remove("fa-check-circle");
-		document.querySelector("#grup__pass2 .formulari__input-error").classList.add("formulari__input-error-actiu");
+		inputPassword2.parentElement.parentElement.classList.add("formulariGrup-incorrecte");
+		inputPassword2.parentElement.parentElement.classList.remove("formulariGrup-correcte");
+		inputPassword2.parentElement.children[1].classList.add("fa-times-circle");
+		inputPassword2.parentElement.children[1].classList.remove("fa-check-circle");
+		inputPassword2.parentElement.parentElement.children[2].classList.add("formulariInput-error-actiu");
 		campos.password = false;
 	} else {
-		document.getElementById("grup__pass2").classList.remove("formulari__grup-incorrecte");
-		document.getElementById("grup__pass2").classList.add("formulari__grup-correcte");
-		document.querySelector("#grup__pass2 i").classList.remove("fa-times-circle");
-		document.querySelector("#grup__pass2 i").classList.add("fa-check-circle");
-		document.querySelector("#grup__pass2 .formulari__input-error").classList.remove("formulari__input-error-actiu");
+		inputPassword2.parentElement.parentElement.classList.remove("formulariGrup-incorrecte");
+		inputPassword2.parentElement.parentElement.classList.add("formulariGrup-correcte");
+		inputPassword2.parentElement.children[1].classList.remove("fa-times-circle");
+		inputPassword2.parentElement.children[1].classList.add("fa-check-circle");
+		inputPassword2.parentElement.parentElement.children[2].classList.remove("formulariInput-error-actiu");
 		campos.password = true;
 	}
+}
+function jaExisteix(usuari, mail) {
+	let existeix = false;
+		for (let [clau, valor] of bbdd) {
+			if ((clau==usuari) || (valor[1]==mail)) {
+				existeix = true;
+			}
+		}
+    return existeix;
 }
 
 // ---------------------------------------------------------------------------------------
 // BOTÓ 2: LOGIN
 // ---------------------------------------------------------------------------------------
-function Login() {
-    var user = document.getElementById("loginuser").value;
-    var pass = document.getElementById("loginpass").value;
-    if (existeixUsuari(user, pass)){
-		login.reset();
-		document.getElementById('login__mensaje').classList.remove('formulari__mensaje-actiu');
-		$('#login-exitos').modal('show');
+function Login(e) {
+	e.preventDefault();
+    if (existeixUsuari(e.target[0].value, e.target[1].value)){
+		//Eliminem els camps i possibles missatges d'error anteriors
+		e.target.reset();
+		e.target.children[0].children[2].classList.remove('formulariMensaje-actiu');
+		//Mostrem la finestra modal
+		$('#registre-exitos').modal('show');
 		$('#login').collapse('hide');
     } else {
-		document.getElementById('login__mensaje').classList.add('formulari__mensaje-actiu');
+		e.target.children[0].children[2].classList.add('formulariMensaje-actiu');
     }
 }
 
